6716a213290255de3a72d938d84a9a8fb8a7ea7d
aijatikuma@inbox.lv <v.smetskaja@gmail.com>
2017-09-27 14:08:41 +0300

    2017/09/27_14:10

---
+++ shell_examples.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#3. piemers
+NAME="Vārds Uzvārds"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2. piemers
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvārds Vārds"
+#echo $NAME
+
+#1. piemers
+#NAME="Vārds Uzvārds"
+echo $NAME
+
+#0.piemers 
+#history > history_20170927.txt
+

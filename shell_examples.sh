94d8814fa273a5a84d844e9298e7e56ec985b795
aijatikuma@inbox.lv <v.smetskaja@gmail.com>
2017-09-27 16:08:54 +0300

    2017/09/27_16:05

--- shell_examples.sh
+++ shell_examples.sh
@@ -1,9 +1,45 @@
 #!/bin/bash
+
+#7.piemers
+
+#vertiba=`expr 2 + 2`
+#echo "summas vērība: $vertiba"
+
+#6. piemers
+#echo $*
+#kartas_numurs=1
+#for arguments in $*
+#do
+#echo $kartas_numurs "arguments -" $arguments
+#kartas_numurs=$kartas_numurs+1
+#done
+
+#5.piemers
+#echo "skriptam nodoto argumentu skaits: " $#
+#echo "argumentu saraksts (attelošana/grupēšana veids 1)" $*
+#echo "argumentu saraksts (attelošana/grupēšana veids 2)" $@
+#echo "Pirmā argumenta vērtība: " $1
+#echo "Otra argumenta vērtība: " $2
+#echo $1$2
+
+
+#4. piemers
+#echo "izpildama skripta faila nosaukums: " $0 
+##echo $n
+#echo "skriptam nodoto argumentu skaits: " $#
+#echo "argumentu saraksts (attelošana/grupēšana veids 1)" $*
+#echo "argumentu saraksts (attelošana/grupēšana veids 2)" $@
+#ech "argumentu saraksts (attelošana/grupēšana veids 2)" $@
+#echo "skripta izpildei piešķirtais procesa numurs" $$
+##echo $!
+#echo "ieprēkšajas komandas izpildes rezultats" $?
+
+
 #3. piemers
-NAME="Vārds Uzvārds"
-echo $NAME
-unset NAME
-echo $NAME
+#NAME="Vārds Uzvārds"
+#echo $NAME
+#unset NAME
+#echo $NAME
 
 #2. piemers
 #NAME="Vārds Uzvārds"
